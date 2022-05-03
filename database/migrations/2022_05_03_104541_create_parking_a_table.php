30ae1e994ec659e0b94b93e82a7b7ebbbdcb85c8
Koki Ishida <ishida@feliseed.co.jp>
2022-05-03 20:21:55 +0900

    [DB] 駐車場A,B,Cの空き状況テーブルを作成

---
+++ database/migrations/2022_05_03_104541_create_parking_a_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParkingATable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parking_a', function (Blueprint $table) {
+            $table->id();
+            $table->integer('status')->comment('{0:不明,1:空,2:混,3:満}');
+            $table->text('description')->nullable();
+            $table->dateTime('scraped_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parking_a');
+    }
+}
