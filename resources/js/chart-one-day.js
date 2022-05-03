c075868887d67ef3fa6a964c217788831c6bde15
Koki Ishida <ishida@feliseed.co.jp>
2022-05-04 00:42:26 +0900

    [js, css] webpack対応

---
+++ resources/js/chart-one-day.js
@@ -0,0 +1,118 @@
+import {
+    Chart,
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip
+  } from 'chart.js';
+  
+  Chart.register(
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip
+  );
+
+const data = {
+    // x軸
+    labels: datetimeAxis(),
+    // y軸
+    datasets: [
+        {
+            label: 'Dataset',
+            data: dummyData(),
+            borderColor: 'rgb(255, 99, 132, 0.8)',
+            backgroundColor: 'rgba(255, 99, 132, 0.8)',
+            fill: false,
+            stepped: true,
+            fill: true,
+        }
+    ]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {
+        responsive: true,
+        interaction: {
+            intersect: false,
+            axis: 'x'
+        },
+        scales: {
+            y: {
+                min: 1,
+                max: 3,
+                ticks: {
+                    stepSize: 1,
+                }
+            },
+        },
+        plugins: {
+            title: {
+                display: true,
+                text: 'C駐車場の１日の空車状況'
+            }
+        }
+    }
+};
+
+function datetimeAxis() {
+    let result = [];
+    for (let i = 0; i < 24; i++) {
+        result.push(`${i}:00`);
+    }
+    return result;
+}
+console.log(datetimeAxis())
+
+function dummyData() {
+    // let response = await axios.get('/parking-c');
+
+    let result = [];
+    for (let i = 0; i < 24; i++) {
+        result.push(Math.floor(Math.random() * 4));
+    }
+    return result;
+}
+console.log(dummyData())
+
+new Chart(document.getElementById('myChart'), config);
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/chart-one-day.js', 'public/js/chart-one-day.js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
