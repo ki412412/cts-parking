d5b50eaa5e8fbda5752a3613b12d9061fb8ea6ef
Koki Ishida <ishida@feliseed.co.jp>
2022-05-03 20:22:44 +0900

    [スクレイピング] スクレイピング結果をDBに登録する処理実装

---
+++ resources/js/puppeteer/config.js
@@ -0,0 +1,16 @@
+exports.config = {
+    url: "https://parking.hokkaido-airports.co.jp/cts/",
+    screenShot: {
+        path: ".",
+        extension: "jpg",
+        quality: 50
+    },
+    mysql: {
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: 'password',
+        database: 'cts_parking',
+        connectionLimit: 10,
+    }
+};
--- resources/js/puppeteer/cts-parking.js
+++ resources/js/puppeteer/cts-parking.js
@@ -1,20 +1,12 @@
 const puppeteer = require('puppeteer');
 const moment = require('moment');
-const config = {
-    url: "https://parking.hokkaido-airports.co.jp/cts/",
-    screenShot: {
-        path: ".",
-        extension: "jpg",
-        quality: 50
-    }
-};
-
-const today = moment().format("YYYYMMDD_HHmmss");
-const screenShot = `${config.screenShot.path}/${today}.${config.screenShot.extension}`;
+const mysql = require('mysql');
+const util = require('util');
+const config = require('./config').config;
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         slowMo: 1000 // [msec]
     })
     const page = await browser.newPage();
@@ -22,7 +14,9 @@ const screenShot = `${config.screenShot.path}/${today}.${config.screenShot.exten
     await page.goto(config.url);
 
     // スクショ
-    await page.screenshot({ path: screenShot, quality: 50, fullPage: true });
+    // const today = moment().format("YYYYMMDD_HHmmss");
+    // const screenShot = `${config.screenShot.path}/${today}.${config.screenShot.extension}`;
+    // await page.screenshot({ path: screenShot, quality: 50, fullPage: true });
 
     // iframeレンダリング完了を待つ
     // const frame = page.frames()[page.frames().length-1]; // FIXME: frameが0の場合の例外処理が必要
@@ -32,12 +26,16 @@ const screenShot = `${config.screenShot.path}/${today}.${config.screenShot.exten
     try {
         const result = await getParkingStatus(page);
         console.log(result);
+        // DBに結果登録
+        await saveToDataBase(result);
     } catch (error) {
         console.log(error);
         await browser.close();
+        process.exit();
     }
     
     await browser.close();
+
 })();
 
 /**
@@ -70,34 +68,34 @@ const screenShot = `${config.screenShot.path}/${today}.${config.screenShot.exten
     //     parkingA: {
     //         status: 1,
     //         description: '入場まで0分待ち',
-    //         datetime: '2022-05-03 19:14:35'
+    //         scraped_at: '2022-05-03 19:14:35'
     //     },
     //     parkingB: {
     //         status: 1,
     //         description: '入場まで30分待ち',
-    //         datetime: '2022-05-03 19:14:35'
+    //         scraped_at: '2022-05-03 19:14:35'
     //     },
     //     parkingC: {
     //         status: 2,
     //         description: '入場まで0分待ち',
-    //         datetime: '2022-05-03 19:14:35'
+    //         scraped_at: '2022-05-03 19:14:35'
     //     }
     // }
     return {
         parkingA: {
             status: a_status,
             description: a_description,
-            datetime: moment().format("YYYY-MM-DD HH:mm:ss")
+            scraped_at: moment().format("YYYY-MM-DD HH:mm:ss")
         },
         parkingB: {
             status: b_status,
             description: b_description,
-            datetime: moment().format("YYYY-MM-DD HH:mm:ss")
+            scraped_at: moment().format("YYYY-MM-DD HH:mm:ss")
         },
         parkingC: {
             status: c_status,
             description: c_description,
-            datetime: moment().format("YYYY-MM-DD HH:mm:ss")
+            scraped_at: moment().format("YYYY-MM-DD HH:mm:ss")
         },
     };
 }
@@ -105,4 +103,49 @@ const screenShot = `${config.screenShot.path}/${today}.${config.screenShot.exten
 async function getPropertyValue(page, selector, property) {
     const elementHandle = await page.$(selector);
     return await (await elementHandle.getProperty(property)).jsonValue();
+}
+
+/**
+ * DBにスクレイピング結果登録
+ */
+async function saveToDataBase(result) {
+    const pool = mysql.createPool(config.mysql);
+    pool.query = util.promisify(pool.query);
+
+    try {
+        await pool.query(
+            "INSERT INTO parking_a SET ?", 
+            {
+                status: result.parkingA.status,
+                description: result.parkingA.description,
+                scraped_at: result.parkingA.scraped_at,
+                created_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+                updated_at: null
+            }
+        );
+        await pool.query(
+            "INSERT INTO parking_b SET ?", 
+            {
+                status: result.parkingB.status,
+                description: result.parkingB.description,
+                scraped_at: result.parkingB.scraped_at,
+                created_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+                updated_at: null
+            }
+        );
+        await pool.query(
+            "INSERT INTO parking_c SET ?", 
+            {
+                status: result.parkingC.status,
+                description: result.parkingC.description,
+                scraped_at: result.parkingC.scraped_at,
+                created_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+                updated_at: null
+            }
+        );
+    } catch (error) {
+        console.log(error);
+    }
+
+    pool.end();
 }
